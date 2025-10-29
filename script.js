b854e8193a0db27e20269f0840c2a5d8de5a8446
Luis Chirinos <luisemilio2006@gmail.com>
2025-10-29 09:22:39 -0400

    añadiendo la estructura HTML para los controles y contenedores de las matrices

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // REFERENCIAS AL DOM
+    const sizeASelect = document.getElementById('sizeA');
+    const sizeBSelect = document.getElementById('sizeB');
+    const matrixAContainer = document.getElementById('matrixA-container');
+    const matrixBContainer = document.getElementById('matrixB-container');
+    const resultContainer = document.getElementById('result-container');
+    const messageArea = document.getElementById('message-area');
+    const scalarInput = document.getElementById('scalar');
+
+});
